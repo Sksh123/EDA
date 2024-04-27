3db1c1d991d2f666d3fd8b2ba106283c83640468
Sksh123 <113050178+Sksh123@users.noreply.github.com>
2024-04-27 08:55:56 +0530

    Update Coordinator.java

--- Coordinator.java
+++ Coordinator.java
@@ -11,35 +11,29 @@ public class Coordinator {
 
     public static void main(String[] args) {
         try {
-            serverSocket = new ServerSocket(12345); // Server socket listening on port 12345
+            serverSocket = new ServerSocket(12345);  
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter the number of participants: ");
             expectedParticipants = scanner.nextInt();
 
-            // Accept participants until the expected number is reached
             System.out.println("\nWaiting for the participants...");
             while (participants.size() < expectedParticipants) {
                 Socket participant = serverSocket.accept();
                 participants.add(participant);
                 System.out.println("Participant " + (participants.size()) + " connected from: " + participant.getRemoteSocketAddress());
 
-                // If expected number of participants is reached, start the prepare phase
                 if (participants.size() == expectedParticipants && !preparePhase) {
                     preparePhase = true;
                     sendPrepareMessage();
                 }
             }
 
-            // Receive responses from participants
             receiveResponsesFromParticipants();
 
-            // Make a decision based on received responses
             makeDecision();
 
-            // Calculate the total sum of numbers from participants
             calculateTotalSum();
 
-            // Close all connections
             closeConnections();
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,7 +79,6 @@ public class Coordinator {
     private static void calculateTotalSum() throws IOException {
         int totalSum = 0;
 
-        // Receive numbers from each participant and calculate the total sum
         for (Socket participant : participants) {
             BufferedReader in = new BufferedReader(new InputStreamReader(participant.getInputStream()));
             int number = Integer.parseInt(in.readLine());
@@ -93,7 +86,6 @@ public class Coordinator {
             System.out.println("Received number " + number + " from participant: " + participant.getRemoteSocketAddress());
         }
 
-        // Display total sum
         System.out.println("\nTotal sum of all numbers received from participants: " + totalSum);
     }
 
